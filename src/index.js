8f01a95f22cc1140ca40007c148314f66e426ad7
Chloe Chong <chloechong.us@gmail.com>
2018-05-01 17:08:45 -0400

    added buildmarker

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Cookie')
+console.log('Cookie');
+
+// Real content starts here!
+const mapboxgl = require("mapbox-gl");
+import { buildMarker } from './marker';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWluaWNobG9lIiwiYSI6ImNqZ280OHU4bTBjMG4yeXBtc3MxaGp1anMifQ.RsJ5GoIvu6BGfO-WS53IZw';
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+// If you want to create a div to add a marker you have to have your own image
+// const htmlFSMarker = document.createElement('div');
+// htmlFSMarker.style.width = '32px';
+// htmlFSMarker.style.height = '39px';
+// htmlFSMarker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+const FSMarker = new mapboxgl.Marker()
+    .setLngLat([-74.009151, 40.705086])
+    .addTo(map);
+
+const empireState = buildMarker('activity', [40.748817, -73.985428]);
+empireState.addTo(map);
---
+++ src/marker.js
@@ -0,0 +1,25 @@
+const mapboxgl = require("mapbox-gl");
+mapboxgl.accessToken = 'pk.eyJ1IjoibWluaWNobG9lIiwiYSI6ImNqZ280OHU4bTBjMG4yeXBtc3MxaGp1anMifQ.RsJ5GoIvu6BGfO-WS53IZw';
+
+export function buildMarker (type, coordinates) {
+    // if (type.toLowerCase() === 'restaurant')
+    const markerType = document.createElement('div');
+    markerType.style.width = '32px';
+    markerType.style.height = '39px';
+    // markerType.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
+    switch (type.toLowerCase()) {
+        case 'restaurant':
+            markerType.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
+        break;
+
+        case 'activity':
+            markerType.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+        break;
+
+        case 'hotel' :
+            markerType.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
+        break;
+    }
+
+    return new mapboxgl.Marker(markerType).setLngLat(coordinates);
+}
